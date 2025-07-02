5629b0d3dcb56c155920ca35d01e92cdd31da51c
YaswanthPalepu <108968159+YaswanthPalepu@users.noreply.github.com>
2025-07-02 19:50:17 +0530

    Update main.tf for directory permissions

--- modules/ssh_key/main.tf
+++ modules/ssh_key/main.tf
@@ -12,4 +12,5 @@ resource "local_file" "private_key" {
   content  = tls_private_key.example.private_key_pem
   filename = var.filename
   file_permission = "0400"
+  directory_permission = "0700"
 }
